cd3f7daf7510081077e1425b1f9c87e686ba9aa3
Clement Marescaux <marcle15@student.westerdals.no>
2018-04-20 22:19:05 +0200

    Convert stack recursive to tail recursive

--- src/main/scala/ru/zhenik/task/narytree/TreeBuilder.scala
+++ src/main/scala/ru/zhenik/task/narytree/TreeBuilder.scala
@@ -1,8 +1,11 @@
 package ru.zhenik.task.narytree
 
 
+import scala.annotation.tailrec
+
+
 object TreeBuilder {
-def build(spans: List[Span]): SpanTree = {
+  def build(spans: List[Span]): SpanTree = {
     val rootId = "0"
     // with validation
     if (spans.isEmpty) throw new IllegalArgumentException("List is empty")
@@ -14,27 +17,66 @@ def build(spans: List[Span]): SpanTree = {
     }
   }
 
+
   def build(spans: List[Span], parentId: String): SpanTree = {
+
     val rootSpan = spans.find(_.parentId.equalsIgnoreCase(parentId))
     rootSpan match {
-      case Some(value) =>
-        val remainingNodes = spans.filterNot(_.spanId.equalsIgnoreCase(value.spanId))
-        buildIter(value, remainingNodes)
+      case Some(root) =>
+        val rootSpanTree = SpanTree(root, List.empty)
+        val sortedRootChildren = findSortedChildrenSpanTrees(root, spans)
+        buildIter(rootSpanTree, sortedRootChildren, sortedRootChildren.size, spans)
       case None => throw new IllegalArgumentException(s"Can not create SpanTree with given list: ${spans.toString}")
     }
   }
 
-  def buildIter(current: Span, spans: List[Span]): SpanTree = {
-    spans.size match {
-      case 0 => SpanTree(current, Nil)
+  @tailrec
+  def buildIter(acc: SpanTree, spanStack: List[SpanTree], childCount: Int, unprocessedSpans: List[Span]): SpanTree = {
+    childCount match {
+      case 0 =>
+        spanStack match {
+          case Nil =>
+            acc // end case: nothing left to process
+          case head :: tail =>
+            // previous root becomes acc again
+            // append current root to children of previous root
+            buildIter(head.copy(children =  head.children :+ acc), tail, findChildrenSpans(head.value, unprocessedSpans).size - head.children.size - 1, unprocessedSpans)
+
+        }
       case _ =>
-        val childrenSpans = getChildrenSpans(current, spans).sortBy(_.startTime)
-        val remaining = spans.diff(childrenSpans)
-        SpanTree(current, childrenSpans.map(s => buildIter(s, remaining)))
+        spanStack match {
+          case Nil =>
+            acc
+          case head :: tail =>
+            val headChildren = findSortedChildrenSpanTrees(head.value, unprocessedSpans)
+            headChildren match {
+              case Nil =>
+                // if has children
+                //    sort them
+                //    add them on stack
+                //    first one becomes accumulator for next recursion
+                buildIter(acc.copy(children = acc.children :+ head), tail, childCount - 1, unprocessedSpans)
+              case child :: others =>
+                // if has no children
+                //    pop and add to children of current accumulator
+                //    update remaing childCount
+                buildIter(head, child :: others ::: acc :: tail, others.size + 1, unprocessedSpans)
+            }
+        }
     }
+
   }
 
-  def getChildrenSpans(span: Span, spans: List[Span]): List[Span] = {
+  def splitChildrenFromRest(mother: Span, pool: List[Span]): (List[Span], List[Span]) = pool.partition(isChildOf(_, mother))
+
+  def intoSpanTree(span: Span): SpanTree = SpanTree(span, Nil)
+
+  def intoSpanTrees(spans: List[Span]): List[SpanTree] = spans.map(intoSpanTree)
+
+  def findSortedChildrenSpanTrees(span: Span, spans: List[Span]): List[SpanTree] =
+    findChildrenSpans(span, spans).sortBy(_.startTime).map(intoSpanTree)
+
+  def findChildrenSpans(span: Span, spans: List[Span]): List[Span] = {
     spans.filter(child => isChildOf(child, span))
   }
 
